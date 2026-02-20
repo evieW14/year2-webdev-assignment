217dfa75ab9b76efcba01f097cfafe62e9f7222d
eviewhite <evieeviewhite@gmai.com>
2026-02-20 23:00:51 +0000

    initial commit of base structure of the webpage

---
+++ home.js
@@ -0,0 +1,40 @@
+window.onload = () => {
+    const galleryImages = [
+        {
+            src: "https://hips.hearstapps.com/hmg-prod/images/beyonce-superbowl-6842f55d6c927.jpg?crop=1.00xw:0.667xh;0,0.207xh&resize=1120:*",
+            alt: "Beyonce Elle Cowboy Carter"
+        },
+        {
+            src: "https://hips.hearstapps.com/hmg-prod/images/entertainer-beyonce-performs-on-stage-during-the-mrs-carter-news-photo-1634153930.jpg?crop=0.748xw:1.00xh;0.0962xw,0&resize=640:*",
+            alt: "Beyonce Elle Life and Career"
+        },
+        {
+            src: "https://imageio.forbes.com/specials-images/imageserve/6760517984921923e8effbcd/0x0.jpg?format=jpg&crop=1736,1737,x834,y79,safe&height=416&width=416&fit=bounds",
+            alt: "Beyonce Forbes"
+        },
+        {
+            src: "https://charts-static.billboard.com/img/2002/06/beyonce-n2a-344x344.jpg",
+            alt: "Beyonce Billboard"
+        }
+    ];
+    
+    renderGallery(galleryImages, "gallery");
+}
+
+function renderGallery(images, elementId) {
+    let output = ""; 
+
+    for (const image of images) { 
+        output += ` 
+        <div class="responsive">
+        <div class="gallery"> 
+            <img src="${image.src}" alt="${image.alt}"> 
+        </div> 
+        </div> 
+        `; 
+    } 
+
+    document.getElementById(elementId).innerHTML = output;
+}
+
+  
