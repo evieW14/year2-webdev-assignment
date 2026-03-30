2e087d6e3af3edfc9a605f1a376f861076149829
evieW14 <evieeviewhite@gmail.com>
2026-03-30 13:21:03 +0100

    Feature/discography tabs (#7)
    * feature/discography-tabs initial tabs implemented for DC and Beyonce
    
    * feature/discography-tabs add category selection and show different options
    
    ---------
    
    Co-authored-by: eviewhite <evieeviewhite@gmai.com>

---
+++ scripts/data/destinys_child.js
@@ -0,0 +1,107 @@
+export const dcAlbumList = [
+        {
+            title: "Destiny's Child",
+            released: "1998",
+            image: "images/Destiny's_Child_1998.png",
+            alt: "Destiny's Child Self-Titled Album Cover",
+            tracks: [
+                "Second Nature",
+                "No, No, No (Part 2)",
+                "With Me (Part I)",
+                "Tell Me",
+                "Bridges",
+                "No, No, No (Part 1)",
+                "With Me (Part II)",
+                "Show Me the Way",
+                "Killing Time",
+                "Illusion",
+                "Birthday",
+                "Sail On",
+                "My Time Has Come"
+            ]
+        },
+        {
+            title: "The Writing's on the Wall",
+            released: "1999",
+            image: "images/The_Writing's_on_the_Wall.png",
+            alt: "Destiny's Child The Writing's on the Wall Album Cover",
+            tracks: [
+                "Intro (The Writing's on the Wall)",
+                "So Good",
+                "Bills, Bills, Bills",
+                "Confessions",
+                "Bug a Boo",
+                "Temptation",
+                "Now That She's Gone",
+                "Where'd You Go",
+                "Hey Ladies",
+                "If You Leave",
+                "Jumpin', Jumpin'",
+                "Say My Name",
+                "She Can't Love You",
+                "Stay",
+                "Sweet Sixteen",
+                "Outro (Amazing Grace)"
+            ]
+        },
+        {
+            title: "Survivor",
+            released: "2001",
+            image: "images/Destiny's_Child_–_Survivor.jpg",
+            alt: "Destiny's Child Survivor Album Cover",
+            tracks: [
+                "Independent Women Part I",
+                "Survivor",
+                "Bootylicious",
+                "Nasty Girl",
+                "Fancy",
+                "Apple Pie à la Mode",
+                "Sexy Daddy",
+                "Independent Women Part II",
+                "Happy Face",
+                "Emotion",
+                "Dangerously in Love",
+                "Brown Eyes",
+                "The Story of Beauty"
+            ]
+        },
+        {
+            title: "8 Days of Christmas",
+            released: "2001",
+            image: "images/Destiny's_Child_-_8_Days_of_Christmas.jpg",
+            alt: "Destiny's Child 8 Days of Christmas Album Cover",
+            tracks: [
+                "8 Days of Christmas",
+                "Winter Paradise",
+                "A DC Christmas Medley",
+                "Silent Night",
+                "Little Drummer Boy",
+                "Do You Hear What I Hear?",
+                "White Christmas",
+                "Platinum Bells",
+                "O' Holy Night",
+                "Spread a Little Love on Christmas Day",
+                "This Christmas"
+            ]
+        },
+        {
+            title: "Destiny Fulfilled",
+            released: "2004",
+            image: "images/Destiny's_Child_–_Destiny_Fulfilled.jpg",
+            alt: "Destiny's Child Destiny Fulfilled Album Cover",
+            tracks: [
+                "Lose My Breath",
+                "Soldier",
+                "Cater 2 U",
+                "T-Shirt",
+                "Is She the Reason",
+                "Girl",
+                "Bad Habit",
+                "If",
+                "Free",
+                "Through with Love",
+                "Love",
+                "Game Over"
+            ]
+        }
+    ];
--- scripts/discography.js
+++ scripts/discography.js
@@ -1,204 +1,61 @@
+import { beyonceAlbumList } from "./data/beyonce.js";
+import { dcAlbumList } from "./data/destinys_child.js";
+
 window.addEventListener("load", () => {
-
-    const albumList = [
-        {
-            title: "Dangerously in Love",
-            released: "2003",
-            image: "images/Dangerously_In_Love_Album.png",
-            alt: "Beyonce Dangerously in Love Album Cover",
-            tracks: [
-              "Crazy in Love",
-              "Naughty Girl",
-              "Baby Boy",
-              "Hip Hop Star",
-              "Be with You",
-              "Me, Myself and I",
-              "Yes",
-              "Signs",
-              "Speechless",
-              "That's How You Like It",
-              "The Closer I Get to You",
-              "Dangerously in Love 2",
-              "Beyoncé Interlude",
-              "Gift from Virgo",
-              "Daddy"
-            ]
-        },
-        {
-            title: "B'Day",
-            released: "2006",
-            image: "images/Beyonce_-_B'Day.png",
-            alt: "Beyonce B'Day Album Cover",
-            tracks: [
-              "Déjà Vu",
-              "Get Me Bodied",
-              "Suga Mama",
-              "Upgrade U",
-              "Ring the Alarm",
-              "Kitty Kat",
-              "Freakum Dress",
-              "Green Light",
-              "Irreplaceable",
-              "Resentment"
-            ]
-        },
-        {
-            title: "I Am... Sasha Fierce",
-            released: "2008",
-            image: "images/I_Am..._Sasha_Fierce.png",
-            alt: "Beyonce I Am... Sasha Fierce Album Cover",
-            tracks: [
-              "If I Were a Boy",
-              "Halo",
-              "Disappear",
-              "Broken-Hearted Girl",
-              "Ave Maria",
-              "Smash into You",
-              "Satellites",
-              "That's Why You're Beautiful",
-              "Single Ladies (Put a Ring on It)",
-              "Radio",
-              "Diva",
-              "Sweet Dreams",
-              "Video Phone",
-              "Hello",
-              "Ego",
-              "Scared of Lonely"
-            ]
-        },
-        {
-            title: "4",
-            released: "2011",
-            image: "images/Beyonce_-_4.png",
-            alt: "Beyonce 4 Album Cover",
-            tracks: [
-              "1+1",
-              "I Care",
-              "I Miss You",
-              "Best Thing I Never Had",
-              "Party",
-              "Rather Die Young",
-              "Start Over",
-              "Love on Top",
-              "Countdown",
-              "End of Time",
-              "I Was Here",
-              "Run the World (Girls)"
-            ]
-        },
-        {
-            title: "Beyonce",
-            released: "2013",
-            image: "images/Beyonce_-_Beyonce.png",
-            alt: "Beyonce Self Titled Album Cover",
-            tracks: [
-              "Pretty Hurts",
-              "Haunted",
-              "Drunk in Love",
-              "Blow",
-              "No Angel",
-              "Partition",
-              "Jealous",
-              "Rocket",
-              "Mine",
-              "XO",
-              "Flawless",
-              "Superpower",
-              "Heaven",
-              "Blue"
-            ]
-        },
-        {
-            title: "Lemonade",
-            released: "2016",
-            image: "images/Beyonce_-_Lemonade.png",
-            alt: "Beyonce Lemonade Album Cover",
-            tracks: [
-              "Pray You Catch Me",
-              "Hold Up",
-              "Don't Hurt Yourself",
-              "Sorry",
-              "6 Inch",
-              "Daddy Lessons",
-              "Love Drought",
-              "Sandcastles",
-              "Forward",
-              "Freedom",
-              "All Night",
-              "Formation"
-            ]
-        },
-        {
-            title: "Renaissance",
-            released: "2022",
-            image: "images/Beyonce_-_Renaissance.png",
-            alt: "Beyonce Renaissance Album Cover",
-            tracks: [
-              "I'm That Girl",
-              "Cozy",
-              "Alien Superstar",
-              "Cuff It",
-              "Energy",
-              "Break My Soul",
-              "Church Girl",
-              "Plastic Off the Sofa",
-              "Virgo's Groove",
-              "Move",
-              "Heated",
-              "Thique",
-              "All Up in Your Mind",
-              "America Has a Problem",
-              "Pure/Honey",
-              "Summer Renaissance"
-            ]
-        },
-        {
-            title: "Cowboy Carter",
-            released: "2024",
-            image: "images/Cowboy_Carter.png",
-            alt: "Beyonce Cowboy Carter Album Cover",
-            tracks: [
-              "Ameriican Requiem",
-              "Blackbiird",
-              "16 Carriages",
-              "Protector",
-              "My Rose",
-              "Smoke Hour ★ Willie Nelson",
-              "Texas Hold 'Em",
-              "Bodyguard",
-              "Dolly P",
-              "Jolene",
-              "Daughter",
-              "Spaghettii",
-              "Alliigator Tears",
-              "Smoke Hour II",
-              "Just for Fun",
-              "II Most Wanted",
-              "Levii's Jeans",
-              "Flamenco",
-              "The Linda Martell Show",
-              "Ya Ya",
-              "Oh Louisiana",
-              "Desert Eagle",
-              "Riiverdance",
-              "II Hands II Heaven",
-              "Tyrant",
-              "Sweet ★ Honey ★ Buckiin'",
-              "Amen"
-            ]
-        },
-    ]
-
-    renderAlbumContent(albumList, "albums");
-
+    renderAlbumContent(dcAlbumList, "DC-albums");
+    renderAlbumContent(beyonceAlbumList, "Beyonce-albums");
 });
 
+// Set currentArtist to be used later
+let currentArtist = null;
+
+// Artist Tab Switching Handler
+function openArtist(evt, artistName) {
+    currentArtist = artistName;
+
+    // Remove active class to hide other tabs
+    document.querySelectorAll(".tabLinks").forEach(btn => {
+        btn.classList.remove("active");
+    });
+
+    // Show selected artist
+    document.querySelectorAll(".tabContent").forEach(sec => sec.classList.remove("active"));
+    document.getElementById(artistName).classList.add("active");
+    evt.currentTarget.classList.add("active");
+
+    // Show correct sub-section
+    const dropdown = document.getElementById("subSelect");
+    openCategory(dropdown.value);
+}
+
+// Category Switching Handler
+function openCategory(category) {
+    if (!currentArtist) return;
+
+    // Hide other sub contents
+    const contents = document.getElementsByClassName("subContent");
+    for (let i = 0; i < contents.length; i++) {
+        contents[i].classList.remove("active");
+    }
+
+    // Create artist + category id
+    const id = `${currentArtist}-${category}`;
+    const section = document.getElementById(id);
+
+    // Display section
+    if (section) {
+        section.classList.add("active");
+    }
+}
+
+// Handle Album Content
 function renderAlbumContent(albums, elementId) {
     const albumElement = document.getElementById(elementId);
     if (!albumElement) return;
 
     let output = "";
 
+    // Create each album objects
     albums.forEach((album, index) => {
         const modalId = `modal-${index}`;
         const btnId = `btn-${index}`;
@@ -231,7 +88,7 @@ function renderAlbumContent(albums, elementId) {
     initAllModals();
 }
 
-
+// Handle creating all track modals
 function initAllModals() {
     const buttons = document.querySelectorAll("button[id^='btn-']");
     const modals = document.querySelectorAll(".modal");
@@ -264,3 +121,11 @@ function initAllModals() {
     });
 }
 
+
+// Load the default tab
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("defaultOpen").click();
+});
+
+window.openArtist = openArtist;
+window.openCategory = openCategory;
